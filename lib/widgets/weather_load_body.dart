e693304b7d7265c1785b4e3a593ed41982ea3f0b
Abdullah Elkholy <72396107+BodyElkholy@users.noreply.github.com>
2024-07-25 03:49:52 +0300

    Update weather_load_body.dart

--- lib/widgets/weather_load_body.dart
+++ lib/widgets/weather_load_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/cubit/weather_cubit.dart';
 import 'package:flutter_application/cubit/weather_icon_cubit.dart';
 import 'package:flutter_application/cubit/weather_icon_states.dart';
+import 'package:flutter_application/main.dart';
 import 'package:flutter_application/models/weather_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,80 +11,98 @@ class WeatherLoadedBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Gets the needed object of the data prepared to be used
     WeatherModel weather = BlocProvider.of<WeatherCubit>(context).weatherModel;
 
-    BlocProvider.of<WeatherIconCubit>(context).getWeatherIcon(
-        iconLink:
-            BlocProvider.of<WeatherCubit>(context).weatherModel.iconImage);
+    // Initializes target widget by passing needed parameters
+    BlocProvider.of<WeatherIconCubit>(context)
+        .getWeatherIcon(iconLink: weather.iconImage);
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Text(
-              weather.cityName,
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(width: 4),
-            Text(
-              weather.countryName,
-              style: TextStyle(fontSize: 12),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            getThemeColor(weather.condition)[500]!,
+            getThemeColor(weather.condition)[300]!,
+            getThemeColor(weather.condition)[50]!,
           ],
         ),
-        Text(
-          'Last updated at: ${weather.time}',
-          style: TextStyle(fontSize: 16),
-        ),
-        SizedBox(height: 24),
-        Row(
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 80,
-              height: 80,
-              child: BlocBuilder<WeatherIconCubit, WeatherIconState>(
-                builder: (context, state) {
-                  if (state is WeatherIconLoading) {
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    // state is WeatherIconLoaded
-                    return BlocProvider.of<WeatherIconCubit>(context)
-                        .weatherIcon;
-                  }
-                },
-              ),
-            ),
-            SizedBox(width: 34),
-            Text(
-              '${weather.temp}${weather.tempType}',
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(width: 38),
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
               children: [
                 Text(
-                  'maxTemp: ${weather.maxTemp}${weather.tempType}',
-                  style: TextStyle(fontSize: 12),
+                  weather.cityName,
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                 ),
+                SizedBox(width: 4),
                 Text(
-                  'minTemp: ${weather.minTemp}${weather.tempType}',
+                  weather.countryName,
                   style: TextStyle(fontSize: 12),
                 ),
               ],
             ),
+            Text(
+              'Last updated at: ${weather.time}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: BlocBuilder<WeatherIconCubit, WeatherIconState>(
+                    builder: (context, state) {
+                      if (state is WeatherIconLoading) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        // state is WeatherIconLoaded
+                        // Gets the needed object of the data prepared to be used
+                        return BlocProvider.of<WeatherIconCubit>(context)
+                            .weatherIcon;
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(width: 36),
+                Text(
+                  '${weather.temp}${weather.tempType}',
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 36),
+                Column(
+                  children: [
+                    Text(
+                      'maxTemp: ${weather.maxTemp}${weather.tempType}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      'minTemp: ${weather.minTemp}${weather.tempType}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 24),
+            Text(
+              weather.condition,
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
-        SizedBox(height: 24),
-        Text(
-          weather.condition,
-          style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-        ),
-      ],
+      ),
     );
   }
 }
