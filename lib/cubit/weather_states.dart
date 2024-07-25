bebb84db0634a83a48a73896fd4f41fb737292cc
Abdullah Elkholy <72396107+BodyElkholy@users.noreply.github.com>
2024-07-25 03:47:35 +0300

    Update weather_states.dart

--- lib/cubit/weather_states.dart
+++ lib/cubit/weather_states.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_application/models/weather_model.dart';
+
 class WeatherState {}
 
 class WeatherInitialState extends WeatherState {}
@@ -6,6 +8,10 @@ class WeatherLoadingState extends WeatherState {}
 
 class LoadingIconState extends WeatherState {}
 
-class WeatherLoadedState extends WeatherState {}
+class WeatherLoadedState extends WeatherState {
+  final WeatherModel weatherModel;
+
+  WeatherLoadedState(this.weatherModel);
+}
 
 class WeatherFailureState extends WeatherState {}
