05afd29409e7f464c8f969be6f13e5c3fabb15e9
Abdullah Elkholy <72396107+BodyElkholy@users.noreply.github.com>
2024-07-25 03:48:34 +0300

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -10,17 +10,24 @@ class GetWeatherService {
 
   Future<WeatherModel> getCurrentWeather(
       {required String cityName, required String temperatureType}) async {
+    const String defaultErrMessage = 'Oops There was an error, try later';
+    const String emptyFieldErrMessage = 'Please enter your city name.';
+
     try {
       Response response = await dio
           .get('$baseURL/forecast.json?key=$apiKey&q=$cityName&days=1');
       return WeatherModel.fromJSON(
           json: response.data, tempType: temperatureType);
     } on DioException catch (e) {
-      final String errorMessage = e.response?.data['error']['message'] ??
-          'Oops There was an error, try later';
+      final String errorMessage =
+          cityName.isEmpty // Checks for empty text field
+              // equivalent to 'e.response.data["error"]["code"] == 1003'
+              ? emptyFieldErrMessage
+              : e.response?.data['error']['message'] ??
+                  'Please check your internet connection'; // nullErrMessage
       throw Exception(errorMessage);
     } catch (e) {
-      throw Exception('Oops There was an error, try later');
+      throw Exception(defaultErrMessage);
     }
   }
 }
